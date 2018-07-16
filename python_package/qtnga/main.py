ceec6910cb23893d4f0e36fa6ee8a2d14e5aae70
icyblade <icyblade.py@gmail.com>
2018-07-16 19:16:21 +0800

    enable high DPI support

--- python_package/qtnga/main.py
+++ python_package/qtnga/main.py
@@ -1,10 +1,13 @@
 import sys
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
+
 from qtnga import QtNGA
 
 
 def main():
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
     app = QApplication(sys.argv)
     window = QtNGA()
     window.show()
